9cf3d050d637971d28fce8d6bfdf4757e42406aa
Robert Kopaczewski <rk@23doors.com>
2020-08-19 18:18:50 +0200

    lint

--- app/controllers/instance.go
+++ app/controllers/instance.go
@@ -53,8 +53,8 @@ func (ctr *Controller) InstanceContext(next echo.HandlerFunc) echo.HandlerFunc {
 		}
 
 		if owner.LastAccess.IsNull() || time.Since(owner.LastAccess.Time) > 12*time.Hour {
-			owner.LastAccess.Set(time.Now())                                                  // nolint: errcheck
-			owner.NoticedAt.Set(nil)                                                          // nolint: errcheck
+			owner.LastAccess.Set(time.Now())                    // nolint: errcheck
+			owner.NoticedAt.Set(nil)                            // nolint: errcheck
 			adminMgr.Update(owner, "last_access", "noticed_at") // nolint: errcheck
 		}
 
